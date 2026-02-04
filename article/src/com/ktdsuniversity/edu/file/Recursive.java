96646009558e6d73a810821df8a0ba04c16dcb11
Rien3844 <kuda3844@gmail.com>
2026-02-04 10:12:58 +0900

    FileFinder

--- article/src/com/ktdsuniversity/edu/file/Recursive.java
+++ article/src/com/ktdsuniversity/edu/file/Recursive.java
@@ -1,5 +1,7 @@
 package com.ktdsuniversity.edu.file;
 
+import java.io.File;
+
 //탐색기 만들기 전 재귀호출 연습
 public class Recursive {
 
@@ -23,7 +25,37 @@ public class Recursive {
 		if (start == 1) {
 			return 1;
 		}
-		return start +  sumToZero(start - 1);
+		return start + sumToZero(start - 1);
+	}
+
+	public void printFiles(File target) { // 파일 탐색기
+
+		if (target.isFile()) {
+			System.out.println(target.getAbsolutePath());
+		} else if (target.isDirectory()) {
+			// 폴더 내부의 목록을 조회.
+			File[] children = target.listFiles();
+			for (File child : children) {
+				this.printFiles(child);
+				// System.out.println(child.getAbsolutePath());
+			}
+		}
+	}
+
+	public void deleteDirectory(File target) {
+
+		if (target.isFile()) {
+			System.out.println(target.getAbsolutePath());
+			target.delete();
+		} else if (target.isDirectory()) {
+			// 폴더 내부의 목록을 조회.
+			File[] children = target.listFiles();
+			for (File child : children) {
+				this.deleteDirectory(child);
+			}
+			System.out.println(target.getAbsolutePath());
+			target.delete();
+		}
 	}
 
 	public static void main(String[] args) {
@@ -32,6 +64,12 @@ public class Recursive {
 		r.printNumber(2000); // 2000 ~ 0
 		int result = r.sumToZero(5);
 		System.out.println(result);
+
+//		File root = new File("C:/DevPrograms");
+//		r.printFiles(root);
+
+		File root = new File("C:\\Users\\User\\Documents\\삭제대상");
+		r.deleteDirectory(root);
 	}
 
 }
